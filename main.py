c5e33d0e48fdc3983afefc57533b90ada7a2db2d
GiselaVillegas <gisela.a.villegas@gmail.com>
2024-04-11 14:44:55 -0400

    Agregando las clases en la pkg

--- main.py
+++ main.py
@@ -106,3 +106,5 @@ def main_menu():
 
 if __name__ == "__main__":
     main_menu()
+    
+    
---
+++ pkt/inn_customer.py
@@ -0,0 +1,5 @@
+
+class Customer:
+    __customer = 1
+    def __init__(self, first_Name, last_name, emial, phone_number )
+        Customer.customer
