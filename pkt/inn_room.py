2ad6147bd71c6cd483f67025ef7d44273431f4a2
GiselaVillegas <gisela.a.villegas@gmail.com>
2024-04-13 14:39:14 -0400

    sift tab

--- pkt/inn_room.py
+++ pkt/inn_room.py
@@ -10,142 +10,142 @@ class Room:
             
             
             
-            def list_rooms():   
-                    try:
-                        # Connect to the database
-                        connection = connectDB()
+    def list_rooms():   
+            try:
+                # Connect to the database
+                connection = connectDB()
 
-                        # Create a cursor object to execute SQL queries
-                        cursor = connection.cursor()
+                # Create a cursor object to execute SQL queries
+                cursor = connection.cursor()
 
-                        # Prepare the SQL query to retrieve all rooms
-                        query = "SELECT * FROM rooms"
+                # Prepare the SQL query to retrieve all rooms
+                query = "SELECT * FROM rooms"
 
-                        # Execute the query
-                        cursor.execute(query)
+                # Execute the query
+                cursor.execute(query)
 
-                        # Fetch all the results
-                        rooms = cursor.fetchall()
+                # Fetch all the results
+                rooms = cursor.fetchall()
 
-                        # Display the results
-                        for room in rooms:
-                            print(room)
+                # Display the results
+                for room in rooms:
+                    print(room)
 
-                        # Close the cursor and connection
-                        cursor.close()
-                        connection.close()
-                    except mysql.connector.Error as error:
-                        print(f"Failed to list rooms: {error}")
-                        
+                # Close the cursor and connection
+                cursor.close()
+                connection.close()
+            except mysql.connector.Error as error:
+                print(f"Failed to list rooms: {error}")
+                
+    
             
-                    
-            def decrease_availability(self):
-                try:
-                            # Connect to the database
-                            connection = connectDB()
-
-                            # Create a cursor object to execute SQL queries
-                            cursor = connection.cursor()
-
-                            # Prepare the SQL query to decrease the availability of a room
-                            query = "UPDATE rooms SET room_availability = room_availability - 1 WHERE id = %s"
-                            values = (self.id,)
-
-                            # Execute the query
-                            cursor.execute(query, values)
-
-                            # Commit the changes to the database
-                            connection.commit()
-
-                            # Close the cursor and connection
-                            cursor.close()
-                            connection.close()
-
-                            print("Room availability decreased successfully!")
-                except mysql.connector.Error as error:
-                            print(f"Failed to decrease room availability: {error}")
-
-            def increase_availability(self):
-                try:
-                            # Connect to the database
-                            connection = connectDB()
-
-                            # Create a cursor object to execute SQL queries
-                            cursor = connection.cursor()
-
-                            # Prepare the SQL query to increase the availability of a room
-                            query = "UPDATE rooms SET room_availability = room_availability + 1 WHERE id = %s"
-                            values = (self.id,)
-
-                            # Execute the query
-                            cursor.execute(query, values)
-
-                            # Commit the changes to the database
-                            connection.commit()
-
-                            # Close the cursor and connection
-                            cursor.close()
-                            connection.close()
-
-                            print("Room availability increased successfully!")
-                except mysql.connector.Error as error:
-                            print(f"Failed to increase room availability: {error}")
-                            
-            def get_total_available_rooms():
-                try:
+    def decrease_availability(self):
+        try:
                     # Connect to the database
                     connection = connectDB()
 
                     # Create a cursor object to execute SQL queries
                     cursor = connection.cursor()
 
-                    # Prepare the SQL query to retrieve the total available rooms
-                    query = "SELECT COUNT(*) FROM rooms WHERE room_availability > 0"
+                    # Prepare the SQL query to decrease the availability of a room
+                    query = "UPDATE rooms SET room_availability = room_availability - 1 WHERE id = %s"
+                    values = (self.id,)
 
                     # Execute the query
-                    cursor.execute(query)
+                    cursor.execute(query, values)
 
-                    # Fetch the result
-                    total_available_rooms = cursor.fetchone()[0]
+                    # Commit the changes to the database
+                    connection.commit()
 
                     # Close the cursor and connection
                     cursor.close()
                     connection.close()
 
-                    return total_available_rooms
-                except mysql.connector.Error as error:
-                    print(f"Failed to get total available rooms: {error}")
+                    print("Room availability decreased successfully!")
+        except mysql.connector.Error as error:
+                    print(f"Failed to decrease room availability: {error}")
+
+    def increase_availability(self):
+        try:
+                    # Connect to the database
+                    connection = connectDB()
+
+                    # Create a cursor object to execute SQL queries
+                    cursor = connection.cursor()
+
+                    # Prepare the SQL query to increase the availability of a room
+                    query = "UPDATE rooms SET room_availability = room_availability + 1 WHERE id = %s"
+                    values = (self.id,)
+
+                    # Execute the query
+                    cursor.execute(query, values)
+
+                    # Commit the changes to the database
+                    connection.commit()
+
+                    # Close the cursor and connection
+                    cursor.close()
+                    connection.close()
+
+                    print("Room availability increased successfully!")
+        except mysql.connector.Error as error:
+                    print(f"Failed to increase room availability: {error}")
                     
-           
-            def check_availability(self):
-                    try:
-                        # Connect to the database
-                        connection = connectDB()
+    def get_total_available_rooms():
+        try:
+            # Connect to the database
+            connection = connectDB()
 
-                        # Create a cursor object to execute SQL queries
-                        cursor = connection.cursor()
+            # Create a cursor object to execute SQL queries
+            cursor = connection.cursor()
 
-                        # Prepare the SQL query to check the availability of a room based on room_type
-                        query = "SELECT room_availability FROM rooms WHERE room_type = %s"
-                        values = (self.room_type,)
+            # Prepare the SQL query to retrieve the total available rooms
+            query = "SELECT COUNT(*) FROM rooms WHERE room_availability > 0"
 
-                        # Execute the query
-                        cursor.execute(query, values)
+            # Execute the query
+            cursor.execute(query)
 
-                        # Fetch the result
-                        availability = cursor.fetchone()
+            # Fetch the result
+            total_available_rooms = cursor.fetchone()[0]
 
-                        # Close the cursor and connection
-                        cursor.close()
-                        connection.close()
+            # Close the cursor and connection
+            cursor.close()
+            connection.close()
 
-                        if availability:
-                            return availability[0] > 0
-                        else:
-                            return False
-                    except mysql.connector.Error as error:
-                        print(f"Failed to check room availability: {error}")
-                        return False
+            return total_available_rooms
+        except mysql.connector.Error as error:
+            print(f"Failed to get total available rooms: {error}")
+            
+    
+    def check_availability(self):
+            try:
+                # Connect to the database
+                connection = connectDB()
+
+                # Create a cursor object to execute SQL queries
+                cursor = connection.cursor()
+
+                # Prepare the SQL query to check the availability of a room based on room_type
+                query = "SELECT room_availability FROM rooms WHERE room_type = %s"
+                values = (self.room_type,)
+
+                # Execute the query
+                cursor.execute(query, values)
+
+                # Fetch the result
+                availability = cursor.fetchone()
+
+                # Close the cursor and connection
+                cursor.close()
+                connection.close()
+
+                if availability:
+                    return availability[0] > 0
+                else:
+                    return False
+            except mysql.connector.Error as error:
+                print(f"Failed to check room availability: {error}")
+                return False
                 
                     
            
